b6613dd07808325a433e9d9cb2ff17b1d23b136e
NoNameA 774 <nonamea774@gmail.com>
2014-09-10 11:39:57 +0900

    im.kayac でYo を受けられるようになりました。

--- yo.rb
+++ yo.rb
@@ -36,6 +36,15 @@ def addFriend(database, username, friend)
   end
 end
 
-def notify(database, username, token_user)
-  # どうしよう
+def notify(database, username, fromUser)
+  notifyImKayac(database, username, fromUser) # とりあえずKayac 決め打ち
+end
+
+def notifyImKayac(database, username, fromUser)
+  # とりあえず無認証決め打ち
+  database.query("SELECT * FROM imkayac WHERE userId='#{database.escape("#{username}")}'").each do |r|
+    uri = URI.parse("http://im.kayac.com/api/post/#{r["kayacId"]}")
+    http = Net::HTTP.new(uri.host)
+    http.post(uri.path, URI.escape("message=[OpenYo]\nYo from #{fromUser}"))
+  end
 end
