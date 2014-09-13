5fbd87041beb43b9857debef29e7be3ab8e970f3
NoNameA 774 <nonamea774@gmail.com>
2014-09-13 12:43:59 +0900

    return if に変更

--- app.rb
+++ app.rb
@@ -28,53 +28,41 @@ module Portfolio
       "please send to /yo/\n"
     end
     post '/yo/' do
-      if params[:api_ver] == "0.1"
-        return Yo::sendYo(@database, params[:api_token], params[:username])
-      end
-      "bad api_ver\n{0.1}\n"
+      return "bad api_ver\n{0.1}\n" if params[:api_ver] != "0.1"
+      Yo::sendYo(@database, params[:api_token], params[:username])
     end
     get '/yo/' do
       redirect "/", 301
     end
     post '/yoall/' do
-      if params[:api_ver] == "0.1"
-        return Yo::yoAll(@database, params[:api_token])
-      end
-      "bad api_ver\n{0.1}\n"
+      return "bad api_ver\n{0.1}\n" if params[:api_ver] != "0.1"
+      Yo::yoAll(@database, params[:api_token])
     end
     get '/friends_count' do
       redirect "/friends_count/", 301
     end
     get '/friends_count/' do
-      if params[:api_ver] == "0.1"
-        return Yo::friends_count(@database, params[:api_token])
-      end
-      "bad api_ver\n{0.1}\n"
+      return "bad api_ver\n{0.1}\n" if params[:api_ver] != "0.1"
+      Yo::friends_count(@database, params[:api_token])
     end
     get '/list_friends' do
       redirect "/list_friends/", 301
     end
     get '/list_friends/' do
-      if params[:api_ver] == "0.1"
-        return Yo::list_friends(@database, params[:api_token])
-      end
-      "bad api_ver\n{0.1}\n"
+      return "bad api_ver\n{0.1}\n" if params[:api_ver] != "0.1"
+      Yo::list_friends(@database, params[:api_token])
     end
     get '/sender/:token' do
       @token = params[:token]
       erb :sender
     end
     post '/create_user/' do
-      if params[:api_ver] == "0.1"
-        return Yo::createUser(@database, params[:username])
-      end
-      "bad api_ver\n{0.1}\n"
+      return "bad api_ver\n{0.1}\n" if params[:api_ver] != "0.1"
+      Yo::createUser(@database, params[:username])
     end
     post '/add_imkayac/' do
-      if params[:api_ver] == "0.1"
-        return Yo::addImkayac(@database, params[:api_token], params[:kayac_id], params[:kayac_pass], params[:kayac_sec])
-      end
-      "bad api_ver\n{0.1}\n"
+      return "bad api_ver\n{0.1}\n" if params[:api_ver] != "0.1"
+      Yo::addImkayac(@database, params[:api_token], params[:kayac_id], params[:kayac_pass], params[:kayac_sec])
     end
   end
 end
