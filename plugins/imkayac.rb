b5b6125b7cbf7752084b58eea9214d1ebdc15f7d
NoNameA 774 <nonamea774@gmail.com>
2014-09-25 21:42:21 +0900

    無認証以外にも通知できるようにした。

--- plugins/imkayac.rb
+++ plugins/imkayac.rb
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
+require 'digest/sha1'
+
 module ImKayac
   def notify(database, username, fromUser)
     # とりあえず無認証決め打ち
     database.query("SELECT * FROM imkayac WHERE userId='#{database.escape("#{username}")}'").each do |r|
       uri = URI.parse("http://im.kayac.com/api/post/#{r["kayacId"]}")
       http = Net::HTTP.new(uri.host)
-      http.post(uri.path, URI.escape("message=[OpenYo]\nYo from #{fromUser}"))
+      body = "[OpenYo]\nYo from #{fromUser}";
+      param = "message=#{body}"
+      if not r["password"].nil?
+        param += "&password=#{r["password"]}"
+      end
+      if not r["seckey"].nil?
+        param += "&sig=#{Digest::SHA1.hexdigest(body + r["seckey"])}"
+      end
+      http.post(uri.path, URI.escape(param))
     end
   end
   module_function :notify
