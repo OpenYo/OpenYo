c962d6007791af96865cfd6c090628f1bfb8a37e
NoNameA 774 <nonamea774@gmail.com>
2014-09-27 11:56:55 +0900

    もう無認証じゃなくても送信できる。

--- plugins/imkayac.rb
+++ plugins/imkayac.rb
@@ -4,7 +4,6 @@ require 'digest/sha1'
 
 module ImKayac
   def notify(database, username, fromUser)
-    # とりあえず無認証決め打ち
     database.query("SELECT * FROM imkayac WHERE userId='#{database.escape("#{username}")}'").each do |r|
       uri = URI.parse("http://im.kayac.com/api/post/#{r["kayacId"]}")
       http = Net::HTTP.new(uri.host)
